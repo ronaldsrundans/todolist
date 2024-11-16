9b4c98258f1b4b2f3dd8f1026cbf9bd795f271c8
Ronny <ronaldsrundans@gmail.com>
2024-11-16 10:49:23 +0200

    Update md4.java
    divide display results now

--- ModProgTeh/md4.java
+++ ModProgTeh/md4.java
@@ -23,7 +23,7 @@ public class DALSKAITLIS {
 	    System.out.println("hello 2="+d[0].y);
 
 */
-		for(DALSKAITLIS tmpD : d) tmpD.display2();
+		for(DALSKAITLIS tmpD : d) tmpD.display();
 
 //		if (d[0].isGreater(d[1])) {dSk = d[0]; d[0] = d[1]; d[1] = dSk; }
 /* ja pirmā daļa ir lielāka par otro, tad
@@ -44,8 +44,8 @@ public class DALSKAITLIS {
 		//System.out.println(d[2].isGreater(d[0]));
 
 		//System.out.println(d[2].isEqual(d[2]));
-		d[0].add(d[1]);
-		//d[0].divide(d[1]);
+		//d[0].add(d[1]);
+		d[0].divide(d[1]);
 
 		} 
 	void add( DALSKAITLIS d ) {
@@ -80,34 +80,45 @@ public class DALSKAITLIS {
     	D=D1.add(D2); 
     	/*System.out.println("D="+D);
     	System.out.println("/");
-    	System.out.println("C="+C);*/
-    	//saīsina
-    	G=D.gcd(C);
-    	D=D.divide(G);
-    	C=C.divide(G);
+    	System.out.println("C="+C);
+
     	System.out.println("D="+D);
     	System.out.println("/");
-    	System.out.println("C="+C);
+    	System.out.println("C="+C);*/
     	//izvelk veselo dalu
     	BigInteger res[]; 
     	res = D.divideAndRemainder(C);
-    	System.out.println("res1 0="+res[0]);
-    	System.out.println("res1 1="+res[1]);
+    	//System.out.println("res1 0="+res[0]);
+    	//System.out.println("res1 1="+res[1]);
     	int sigvalue = res[0].signum();
-    	System.out.println("sigvalue="+sigvalue);
+    	//System.out.println("sigvalue="+sigvalue);
     	if (sigvalue ==0 )//nav veselie
-    	{
+    	{    	
+    		//saīsina
+    		G=D.gcd(C);
+    		D=D.divide(G);
+    		C=C.divide(G);
     	   	tmp3x=D.toString();
         	tmp3y=C.toString();
     	}
     	else //ir veselie
     	{
-    		/*if (sigvalue ==(-1) ) //negativi
-    		{
-    			tmp3x="-";
+    		/*
+        		
+    		if (Ay.compareTo(BigInteger.valueOf(1))!=0) {/// saucējs nav 1
+    					tmp2x=tmp2x+"_";
+    			    	tmp2x=tmp2x+Ax.toString();
+    			    	tmp2y=Ay.toString();
     		}*/
+ 
     		tmp3x=res[0].toString(); //+"_";//veselie
     		D=D.subtract(C.multiply(res[0]));
+      		//mīnuss ir tikai pie veselā
+    		D=D.multiply(BigInteger.valueOf(sigvalue));
+    		//saīsina
+    		G=D.gcd(C);
+    		D=D.divide(G);
+    		C=C.divide(G);
     		if (C.compareTo(BigInteger.valueOf(1))!=0) {/// saucējs nav 1
     					tmp3x=tmp3x+"_";
     			    	tmp3x=tmp3x+D.toString();
@@ -117,13 +128,13 @@ public class DALSKAITLIS {
     		    			
     	}
     	//izdruka
+
     	if (C.compareTo(BigInteger.valueOf(1))==0) {
         	System.out.println(tmp3x);
     	}
     	else {
     	System.out.print(tmp3x);
     	System.out.print("/");
-
     	System.out.println(tmp3y);
     	}
 	
@@ -151,24 +162,63 @@ public class DALSKAITLIS {
     	D=D.multiply(BigInteger.valueOf(sigvalue));
     	C=C.multiply(BigInteger.valueOf(sigvalue));
     	
-    	System.out.println("D="+D);
+    	/*System.out.println("D="+D);
     	System.out.println("/");
-    	System.out.println("C="+C);
-    	//saīsina
-    	G=D.gcd(C);
-    	D=D.divide(G);
-    	C=C.divide(G);
-    	
-    	System.out.println("D="+D);
-    	System.out.println("/");
-    	System.out.println("C="+C);
+    	System.out.println("C="+C);*/
+
     	//izvelk veselo dalu
     	//BigInteger res[]; 
     	res = D.divideAndRemainder(C);
-    	System.out.println("res1 0="+res[0]);
-    	System.out.println("res1 1="+res[1]);
+    	//System.out.println("res1 0="+res[0]);
+    	//System.out.println("res1 1="+res[1]);
     	sigvalue = res[0].signum();
-    	System.out.println("sigvalue="+sigvalue);
+    	//System.out.println("sigvalue="+sigvalue);
+    	if (sigvalue ==0 )//nav veselie
+    	{    	
+    		//saīsina
+    		G=D.gcd(C);
+    		D=D.divide(G);
+    		C=C.divide(G);
+    	   	tmp3x=D.toString();
+        	tmp3y=C.toString();
+    	}
+    	else //ir veselie
+    	{
+    		/*
+        		
+    		if (Ay.compareTo(BigInteger.valueOf(1))!=0) {/// saucējs nav 1
+    					tmp2x=tmp2x+"_";
+    			    	tmp2x=tmp2x+Ax.toString();
+    			    	tmp2y=Ay.toString();
+    		}*/
+ 
+    		tmp3x=res[0].toString(); //+"_";//veselie
+    		D=D.subtract(C.multiply(res[0]));
+      		//mīnuss ir tikai pie veselā
+    		D=D.multiply(BigInteger.valueOf(sigvalue));
+    		//saīsina
+    		G=D.gcd(C);
+    		D=D.divide(G);
+    		C=C.divide(G);
+    		if (C.compareTo(BigInteger.valueOf(1))!=0) {/// saucējs nav 1
+    					tmp3x=tmp3x+"_";
+    			    	tmp3x=tmp3x+D.toString();
+    			    	tmp3y=C.toString();
+    		}
+
+    		    			
+    	}
+    	//izdruka
+
+    	if (C.compareTo(BigInteger.valueOf(1))==0) {
+        	System.out.println(tmp3x);
+    	}
+    	else {
+    	System.out.print(tmp3x);
+    	System.out.print("/");
+    	System.out.println(tmp3y);
+    	}
+    	
 	}
 	boolean isEqual(DALSKAITLIS d) { 
 		System.out.println("isEqual?");
@@ -269,124 +319,8 @@ public class DALSKAITLIS {
     	
 		}
 
+
 	public void display() { 
-		System.out.println("Display this!");
-		System.out.println("skait="+x);
-		System.out.println("sauc="+y);
-    	String tmp1x=this.x;
-    	String tmp1y=this.y;
-    	String tmp2x="";
-    	String tmp2y="";
-    	int n=0, negx=0, negy=0;//, neg=0;
-    	//int		ves=0; ///Izdzēst pārveido par BigInt
-    	BigInteger Ax, Ay, B,C, D, E, F;
-    	Ax  = new BigInteger(tmp1x);
-    	Ay = new BigInteger(tmp1y);
-    	B = new BigInteger("0");
-    	C = new BigInteger("0");
-    
-    	D = new BigInteger("0");   	
-    	E = BigInteger.valueOf(1);
-    	F =new BigInteger("0");
-    	
-    	int sigvalue = Ax.signum();
-    	int sigvalue2;
-    	if(sigvalue<0) {
-    		//System.out.println("neg tmp2x"+sigvalue);
-    		negx=1;
-    		Ax=Ax.multiply(BigInteger.valueOf(-1));
-    		//tmp2x=tmp2x.substring(1);
-    	}
-    	sigvalue = Ay.signum();
-    	if(sigvalue<0) {
-    		//System.out.println("neg tmp2y"+sigvalue);
-    		negy=1;
-    		Ay=Ay.multiply(BigInteger.valueOf(-1));
-    		//tmp2y=tmp2y.substring(1);
-    	}
-    	
-    	
-    	
-    	BigInteger res[]; 
-    	//res[0]=BigInteger.valueOf(0);
-    	//res[1]=BigInteger.valueOf(0);
-    	res = Ax.divideAndRemainder(Ay);
-    	//System.out.println("Quotient = " + res[0]+"\nRemainder = " + res[1]); 
-
-    	sigvalue2 = res[0].signum();
- 
-    	if(sigvalue2!=0) {
-    		Ax=res[1];
-    	}
-
-    	
-    	D=Ax.gcd(Ay);
-    	//System.out.println("GCD= "+D);
-    	B=Ax.divide(D);
-    	C=Ay.divide(D);
-    	//F=Ax.divideAndRemainder(Ay);
-    	//System.out.println("Bvalue= "+B);
-    	//System.out.println("Cvalue= "+C);
-
-    	tmp2x=B.toString();
-    	tmp2y=C.toString();
-    	//System.out.println("tmp2x="+tmp2x);
-    	//System.out.println("tmp2y="+tmp2y);
-    	
-
-    	
-    	
-    	//System.out.println(tmp2y.getClass());
-    	//System.out.println("this value="+tmp2y);
-    	//if(y=="1") {
-    	//System.out.println("tmp2= "+tmp2y);
-    	//System.out.println("tmp2 len= "+tmp2y.length());
-    	/*if(tmp2y.length()==1) {
-    		n=Integer.parseInt(tmp2y.substring(0, tmp2y.length()));
-    		}
-        if(n==1 && tmp2y.length()==1) {*/
-        if (C.compareTo(BigInteger.valueOf(1))==0) {
-    		//System.out.println("Enter IF");
-        	if(negx != negy) {
-    			System.out.print("-");
-    		}
-        	/*if(ves>0) {
-        		System.out.println(ves);
-
-        	}*/
-        	if (sigvalue2!=0) {
-        		tmp2x=res[0].toString();
-        		System.out.println(tmp2x);
-        	}
-        	else {
-        		System.out.println("0");
-        	}
-
-    		//System.out.println("sauc ir 1"+tmp2x);
-    		//System.out.println(tmp2x);
-    	}
-        else {
-        	//System.out.println("Enter ELSE");
-        	
-        	
-        	if(negx != negy) {
-        		System.out.print("-");
-        	}
-        	//if(ves>0) {
-        	if (sigvalue2!=0) {
-        		
-        		System.out.print(res[0].toString()+"_");
-
-        	}
-
-    		System.out.println(tmp2x+"/"+tmp2y);
-    		//System.out.println(tmp2y);
-
-
-        }
-
-	}
-	public void display2() { 
 		System.out.println("Display this!");
 	//	System.out.println("skait="+x);
 		//System.out.println("sauc="+y);
