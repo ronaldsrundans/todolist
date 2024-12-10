5eb6ae0789766cf4eb013926974f5fafd1d6b95a
Ronny <ronaldsrundans@gmail.com>
2024-12-10 11:59:50 +0200

    Update futbols.py
    Added Rinkis column in team table

--- ModProgTeh/futbols.py
+++ ModProgTeh/futbols.py
@@ -8,9 +8,10 @@ connection_obj = sqlite3.connect('team.db')
 # cursor object
 cursor_obj = connection_obj.cursor()
  
-# Drop the GEEK table if already exists.
+# Drop the table if already exists.
 cursor_obj.execute("DROP TABLE IF EXISTS TEAM")
- 
+cursor_obj.execute("DROP TABLE IF EXISTS PLAYER")
+
 # Creating table
 table = """ CREATE TABLE TEAM (
             Name VARCHAR(255) NOT NULL,
@@ -20,11 +21,32 @@ table = """ CREATE TABLE TEAM (
             UZSKPL INT,      
             ZSKPL INT,       
             IEGV INT,       
-            ZAUV INT
+            ZAUV INT,
+            RINKIS VARCHAR(255)
         ); """
- 
+#RINKIS Name VARCHAR(255)
+"""turnīra desmit rezultatīvāko spēlētāju (sakārtoti pēc gūto vārtu skaita un 
+rezultatīvo piespēļu skaita dilstošā secībā) saraksts. Jānorāda vieta sarakstā pēc kārtas, 
+spēlētāja vārds un uzvārds, komandas nosaukums, gūto vārtu skaits un rezultatīvo piespēļu skaits. 
+Sarakstā augstāk jāatrodas spēlētājam, kas guvis vairāk vārtu, bet, vienāda gūto vārtu 
+skaita gadījumā, tam spēlētājam, kas vairāk reižu rezultatīvi piespēlējis."""
 cursor_obj.execute(table)
- 
+table2 = """ CREATE TABLE PLAYER (
+            NR INT,            
+            KOM_NOS VARCHAR(255) NOT NULL,
+            VARDS VARCHAR(255) NOT NULL,
+            UZVARDS VARCHAR(255) NOT NULL,
+            VARTI INT,
+            PIESP INT
+         ); """  
+         #
+          #  
+          #  ,
+          
+         #VARTI INT,
+          #  PIESP INT
+cursor_obj.execute(table2)
+
 #print("Table is Ready")
  
 # Close the connection
@@ -46,34 +68,49 @@ class Team:
     self.vpm=0
     self.vpl=0
 #END of class Team
-def my_function(i, path2):
-  #print("Hello from a function")
-  #for i in range (0,3):
-  #print(i)
-  
+class Player:
+  def __init__(self, Nr, Vards, Uzvards, Kom_Nos):
+    self.Nr = Nr
+    self.Vards = Vards
+    self.Uzvards = Uzvards
+    self.Kom_Nos = Kom_Nos
+    self.GVSK=0
+    self.RPSK=0
+
+    #     GVSK       RPSK
+#END of class Player
+def my_function(i, path2,rinkis):
   tree = ET.parse(path2+'futbols'+str(i)+'.xml')
   root = tree.getroot()
   #print(root.tag)
   #print(root.attrib)
-  #komarr=[]
-  #vgarr=[]
   tmp=[]
   nrpk=0
+  person=[]
   #print("Neighbor2:")
   for komanda in root.iter('Komanda'):
-    #print(komanda.attrib)
-    #print("Komandas nosaukums ir "+komanda.get("Nosaukums"))
-    #vgarr.append(komanda.get("Nosaukums"))
-    #komarr.append(komanda.get("Nosaukums"))
     tmp.append(Team(komanda.get("Nosaukums")))
     nrpk+=1
-
-
     for speletaji in komanda.iter('Speletaji'):
          for speletajs in speletaji.iter('Speletajs'):
             #print(ighbor.attrib)
-            rank = speletajs.get ("Uzvards")
-            name = speletajs.get("Vards")
+            surname = speletajs.get ("Uzvards")
+            personname = speletajs.get("Vards")
+            playernr=speletajs.get("Nr")
+            playerteam=tmp[nrpk-1].name
+            person.append(Player(playernr, personname, surname, playerteam))
+            # Connecting to sqlite 
+            conn = sqlite3.connect('team.db') 
+            cursor = conn.cursor() 
+            #for p in person:
+            #print(p.Nr+" "+p.Vards+" "+p.Uzvards+" "+p.Kom_Nos)
+            cursor.execute("INSERT INTO PLAYER VALUES ("+playernr+",'"+playerteam+"','"+personname+"','"+surname+"',0,0)")
+
+            #cursor.execute("INSERT INTO PLAYER VALUES ("+p.Nr+",'"+p.Kom_Nos+"','"+p.Vards+"','"+p.Uzvards+"',0,0)")
+            #,'"+p.Kom_Nos+"','"+p.Vards+"','"+p.Uzvards+"')") 
+            conn.commit() 
+            conn.close()
+
             #print(name, rank)
     #print("_ Komandas sodi: ")
     """
@@ -84,26 +121,33 @@ def my_function(i, path2):
     #print("_ Komandas varti: ")
     for varti in komanda.iter('Varti'):
         for vg in varti.iter('VG'):
-            #print(vg.attrib)
-            #vgarr.append(vg.get("Laiks"))
-
-            #print vg.get("Laiks")
             a = vg.get("Laiks").split(":")#"Hello, World!"
-            #print(a) # returns ['Hello', ' World!'] 
             if(int(a[0])>59 and int(a[1])>1):
               #print("Papildlaiks!")
-              #tmp[nrpk-1].UZSKPL+=1
               tmp[nrpk-1].IEGV+=1
               tmp[nrpk-1].vpl+=1
             else:
               #print("Pamatlaiks!")
-              #tmp[nrpk-1].USKPM+=1
               tmp[nrpk-1].IEGV+=1
               tmp[nrpk-1].vpm+=1
-            #vgarr.append(vg.get("Laiks"))
-    #print("Statistics:")
+            print("Vārtus iesita NR.="+vg.get("Nr"))
+            #for p in vg.iter('P'):
+            #  print(p.get("Nr"))
     if (len(tmp)>1):
       #print("Ieliek zaudētie vārti:")
+      #speletaji
+      """
+      # Connecting to sqlite 
+      conn = sqlite3.connect('team.db') 
+      cursor = conn.cursor() 
+      for p in person:
+        print(p.Nr+" "+p.Vards+" "+p.Uzvards+" "+p.Kom_Nos)
+        cursor.execute("INSERT INTO PLAYER VALUES ("+p.Nr+",'"+p.Kom_Nos+"','"+p.Vards+"','"+p.Uzvards+"',0,0)")
+        #,'"+p.Kom_Nos+"','"+p.Vards+"','"+p.Uzvards+"')") 
+      conn.commit() 
+      conn.close()
+      """
+      # end of speletaji#
       tmp[0].ZAUV+=(tmp[1].vpl+tmp[1].vpm )
       tmp[1].ZAUV+=(tmp[0].vpl+tmp[0].vpm )
       #print("Ieliek punkti:")
@@ -138,181 +182,103 @@ def my_function(i, path2):
     # Connecting to sqlite 
   conn = sqlite3.connect('team.db') 
   cursor = conn.cursor() 
-  cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[0].name+"', '"+str(tmp[0].PUN_TOT)+"', '"+str(tmp[0].USKPM)+"','"+str(tmp[0].ZSKPM)+"','"+str(tmp[0].UZSKPL)+"','"+str(tmp[0].ZSKPL)+"','"+str(tmp[0].IEGV)+"','"+str(tmp[0].ZAUV)+"')") 
-  cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[1].name+"', '"+str(tmp[1].PUN_TOT)+"', '"+str(tmp[1].USKPM)+"','"+str(tmp[1].ZSKPM)+"','"+str(tmp[1].UZSKPL)+"','"+str(tmp[1].ZSKPL)+"','"+str(tmp[1].IEGV)+"','"+str(tmp[1].ZAUV)+"')") 
+  cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[0].name+"', '"+str(tmp[0].PUN_TOT)+"', '"+str(tmp[0].USKPM)+"','"+str(tmp[0].ZSKPM)+"','"+str(tmp[0].UZSKPL)+"','"+str(tmp[0].ZSKPL)+"','"+str(tmp[0].IEGV)+"','"+str(tmp[0].ZAUV)+"','"+rinkis+"')") 
+  cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[1].name+"', '"+str(tmp[1].PUN_TOT)+"', '"+str(tmp[1].USKPM)+"','"+str(tmp[1].ZSKPM)+"','"+str(tmp[1].UZSKPL)+"','"+str(tmp[1].ZSKPL)+"','"+str(tmp[1].IEGV)+"','"+str(tmp[1].ZAUV)+"', '"+rinkis+"')") 
   conn.commit() 
   conn.close()
-
 #ENDof my_function()
 
+def my_function2(where):
+  #print("Hello from a function")
+  # Connecting to sqlite 
+  conn = sqlite3.connect('team.db') 
+  # Creating a cursor object using the  
+  # cursor() method 
+  cursor = conn.cursor() 
+  data3=cursor.execute("SELECT name,sum(PUN_TOT), sum(USKPM), sum(ZSKPM), sum(UZSKPL), sum(ZSKPL), sum(IEGV), sum(ZAUV) FROM TEAM "+where+" group by name order by sum(PUN_TOT) desc")
+  print(cursor.fetchall())
+  # Commit your changes in the database     
+  conn.commit() 
+  # Closing the connection 
+  conn.close()
 
+#END of my_function2()
+def my_function3():
+  #print("Hello from a function")
+  # Connecting to sqlite 
+  conn = sqlite3.connect('team.db') 
+  # Creating a cursor object using the  
+  # cursor() method 
+  cursor = conn.cursor() 
+  data3=cursor.execute("SELECT * FROM PLAYER")
+  #print(cursor.fetchall())
+  # Commit your changes in the database     
+  conn.commit() 
+  # Closing the connection 
+  conn.close()
+#END of my_function3()
+def my_function4(nr, iegv, rpsk):
+  #print("Hello from a function")
+  # Connecting to sqlite 
+  conn = sqlite3.connect('team.db') 
+  # Creating a cursor object using the  
+  # cursor() method 
+  cursor = conn.cursor() 
+  #data3=cursor.execute("SELECT * FROM PLAYER")
+  data3=cursor.execute("UPDATE FROM PLAYER")
 
+  #print(cursor.fetchall())
+  # Commit your changes in the database     
+  conn.commit() 
+  # Closing the connection 
+  conn.close()
+#END of my_function4()
+def addvarti_function4(nr):
+  #print("Hello from a function")
+  # Connecting to sqlite 
+  conn = sqlite3.connect('team.db') 
+  # Creating a cursor object using the  
+  # cursor() method 
+  cursor = conn.cursor() 
+  playerdata=cursor.execute("SELECT * FROM PLAYER WHERE NR="+str(nr)+" ")
+  #data3=cursor.execute("UPDATE FROM PLAYER")
+
+  print(cursor.fetchall())
+  print("last:")
+  for row in playerdata: 
+    print(row) 
+  #print(playerdata['NAME'])
+  #print(playerdata[0])
+  # Commit your changes in the database     
+  conn.commit() 
+  # Closing the connection 
+  conn.close()
+#END of my_function4()
 
 #main
 path="/home/ubuntu/Documents/ModProgrMet/PD2/XML_TestData/XMLFirstRound/"
-
+w1=" WHERE RINKIS='1.rinkis'"
+print("1.rinkis")
 for i in range (0,3):
   #print(i)
-  my_function(i, path)
+  my_function(i, path,'1.rinkis')
+my_function2(w1)
+print("2.rinkis")
 path1="/home/ubuntu/Documents/ModProgrMet/PD2/XML_TestData/XMLSecondRound/"
+w2=" WHERE RINKIS='2.rinkis'"
 for i in range (0,3):
   #print(i)
-  my_function(i, path1)
+  my_function(i, path1,'2.rinkis')
+my_function2(w2)
+#w3=" WHERE RINKIS='1.rinkis' AND WHERE RINKIS='2.rinkis'"
+#my_function2(w3)
+print("1.un 2.rinkis")
+my_function2("")
+
+my_function3()
+addvarti_function4(47)
+
 #END of main
-
-  """
-  #
-  tree = ET.parse(path+'futbols'+str(i)+'.xml')
-  root = tree.getroot()
-  print(root.tag)
-  print(root.attrib)
-  komarr=[]
-  vgarr=[]
-  tmp=[]
-  nrpk=0
-  print("Neighbor2:")
-  for komanda in root.iter('Komanda'):
-    print(komanda.attrib)
-    print("Komandas nosaukums ir "+komanda.get("Nosaukums"))
-    vgarr.append(komanda.get("Nosaukums"))
-    komarr.append(komanda.get("Nosaukums"))
-    tmp.append(Team(komanda.get("Nosaukums")))
-    nrpk+=1
-
-
-    for speletaji in komanda.iter('Speletaji'):
-         for speletajs in speletaji.iter('Speletajs'):
-            #print(ighbor.attrib)
-            rank = speletajs.get ("Uzvards")
-            name = speletajs.get("Vards")
-            #print(name, rank)
-    print("_ Komandas sodi: ")
-    for sodi in komanda.iter('Sodi'):
-        for sods in sodi.iter('Sods'):
-            print(sods.attrib)
-    print("_ Komandas varti: ")
-    for varti in komanda.iter('Varti'):
-        for vg in varti.iter('VG'):
-            print(vg.attrib)
-            vgarr.append(vg.get("Laiks"))
-
-            #print vg.get("Laiks")
-            a = vg.get("Laiks").split(":")#"Hello, World!"
-            print(a) # returns ['Hello', ' World!'] 
-            if(int(a[0])>59 and int(a[1])>1):
-              print("Papildlaiks!")
-              #tmp[nrpk-1].UZSKPL+=1
-              tmp[nrpk-1].IEGV+=1
-              tmp[nrpk-1].vpl+=1
-            else:
-              print("Pamatlaiks!")
-              #tmp[nrpk-1].USKPM+=1
-              tmp[nrpk-1].IEGV+=1
-              tmp[nrpk-1].vpm+=1
-            #vgarr.append(vg.get("Laiks"))
-    print("Statistics:")
-    if (len(tmp)>1):
-      print("Ieliek zaudētie vārti:")
-      tmp[0].ZAUV+=(tmp[1].vpl+tmp[1].vpm )
-      tmp[1].ZAUV+=(tmp[0].vpl+tmp[0].vpm )
-      print("Ieliek punkti:")
-      if(tmp[0].vpl>0 or tmp[1].vpl>0):
-        print("Papildlaika uzvareja:")
-        if(tmp[0].vpl>tmp[1].vpl):
-          tmp[0].PUN_TOT+=3
-          tmp[1].PUN_TOT+=2
-          tmp[0].UZSKPL+=1
-          tmp[1].ZSKPL+=1
-        else:
-          tmp[0].PUN_TOT+=2
-          tmp[1].PUN_TOT+=3
-          tmp[1].UZSKPL+=1
-          tmp[0].ZSKPL+=1
-      else:
-        print("Pamatlaika uzvareja:")
-        if(tmp[0].vpm>tmp[1].vpm):
-          tmp[0].PUN_TOT+=5
-          tmp[1].PUN_TOT+=1
-          tmp[0].USKPM+=1
-          tmp[1].ZSKPM+=1
-        else:
-          tmp[0].PUN_TOT+=1
-          tmp[1].PUN_TOT+=5
-          tmp[1].USKPM+=1
-          tmp[0].ZSKPM+=1
-              
-
-
-    print("Statistics:")
-    # Connecting to sqlite 
-  conn = sqlite3.connect('team.db') 
-  cursor = conn.cursor() 
-  cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[0].name+"', '"+str(tmp[0].PUN_TOT)+"', '"+str(tmp[0].USKPM)+"','"+str(tmp[0].ZSKPM)+"','"+str(tmp[0].UZSKPL)+"','"+str(tmp[0].ZSKPL)+"','"+str(tmp[0].IEGV)+"','"+str(tmp[0].ZAUV)+"')") 
-  cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[1].name+"', '"+str(tmp[1].PUN_TOT)+"', '"+str(tmp[1].USKPM)+"','"+str(tmp[1].ZSKPM)+"','"+str(tmp[1].UZSKPL)+"','"+str(tmp[1].ZSKPL)+"','"+str(tmp[1].IEGV)+"','"+str(tmp[1].ZAUV)+"')") 
-  conn.commit() 
-  conn.close()
-
-
-    #print("Statistics:")
-
-  for x in vgarr:
-    print(x)
-#ENDof for input files
-"""
-#
-#
-#     
-"""
-print("TMP:  ")
-for y in tmp:
-  print(y.name)
-  print("Iegutie vārti:")
-  print(y.IEGV)
-  print("Zaudētie vārti:")
-  print(y.ZAUV)
-"""
-# Connecting to sqlite 
-conn = sqlite3.connect('team.db') 
-  
-# Creating a cursor object using the  
-# cursor() method 
-cursor = conn.cursor() 
-""" 
-data1=cursor.execute("SELECT * FROM TEAM WHERE NAME = '"+tmp[0].name+"'")
-print(cursor.fetchall())
-
-data2=cursor.execute("SELECT * FROM TEAM WHERE NAME = '"+tmp[1].name+"'")
-print(cursor.fetchall())
-data2=cursor.execute("SELECT * FROM TEAM WHERE NAME = 'Barcelona' ")
-print(cursor.fetchall())"""
-data3=cursor.execute("SELECT name,sum(PUN_TOT), sum(USKPM), sum(ZSKPM), sum(UZSKPL), sum(ZSKPL), sum(IEGV), sum(ZAUV) FROM TEAM group by name order by sum(PUN_TOT) desc")
-print(cursor.fetchall())
-# Queries to INSERT records. 
-#cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[0].name+"', '"+str(tmp[0].PUN_TOT)+"', '"+str(tmp[0].USKPM)+"','"+str(tmp[0].ZSKPM)+"','"+str(tmp[0].UZSKPL)+"','"+str(tmp[0].ZSKPL)+"','"+str(tmp[0].IEGV)+"','"+str(tmp[0].ZAUV)+"')") 
-#cursor.execute("INSERT INTO TEAM VALUES ('"+tmp[1].name+"', '"+str(tmp[1].PUN_TOT)+"', '"+str(tmp[1].USKPM)+"','"+str(tmp[1].ZSKPM)+"','"+str(tmp[1].UZSKPL)+"','"+str(tmp[1].ZSKPL)+"','"+str(tmp[1].IEGV)+"','"+str(tmp[1].ZAUV)+"')") 
-# 'UPDATE TEAM SET  = ,  = WHERE NAME="+tmp[0].name+" '
-"""
-data1=cursor.execute("SELECT * FROM TEAM WHERE NAME = '"+tmp[0].name+"'")
-
-print(cursor.fetchall())
-data2=cursor.execute("SELECT * FROM TEAM WHERE NAME = '"+tmp[1].name+"'")
-for row in data2:
-  print("ROW="+str(row[6]))
-print(cursor.fetchall())
-
-
-  
-# Display data inserted 
-print("Data Inserted in the table: ") 
-data=cursor.execute('''SELECT * FROM TEAM''') 
-for row in data: 
-    print(row) 
-"""  
-# Commit your changes in the database     
-conn.commit() 
-  
-# Closing the connection 
-conn.close()
 
 
 
